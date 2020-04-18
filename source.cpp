546342b515728953a34e3a4cfa69a5774ed7571f
Adam K. Zupancic <adamkxz92@pm.me>
2020-04-18 09:31:28 -0700

    source code added

---
+++ source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+
+    int number_of_words = 0;
+    std::string previous = " ";
+    std::string current;
+    while (std::cin >> current) {
+        ++number_of_words;
+        if (previous == current)
+            std::cout << "word number " << number_of_words
+            << " repeated: " << current << std::endl;
+        previous = current;
+    }
+
+}
